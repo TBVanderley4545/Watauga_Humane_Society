7316799c36a8fef93d8e54211e16d72273920529
tylervanderley <tbvanderley@gmail.com>
2016-04-24 22:27:24 -0400

    | Creation of an AsyncTask as an inner class in MainActivity | | Working on parsing in data using JSoup and finding a way to not need an ArrayList | | No longer need ArrayList |

--- app/src/main/java/com/tbvanderleystudios/wataugahumanesociety/MainActivity.java
+++ app/src/main/java/com/tbvanderleystudios/wataugahumanesociety/MainActivity.java
@@ -3,11 +3,24 @@ package com.tbvanderleystudios.wataugahumanesociety;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
 
 import layout.AnimalRecyclerFragment;
 
 public class MainActivity extends Activity {
+
+    private Animal[] mAnimals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +32,56 @@ public class MainActivity extends Activity {
         ft.addToBackStack(null);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         ft.commit();
+
+        new GetAnimalsTask().execute();
+    }
+
+    private class GetAnimalsTask extends AsyncTask<Void, Void, Void> {
+
+        private String mURLAddress = "http://wataugahumane.org/animals";
+        private int mAnimalCount;
+        private ProgressDialog mProgressDialog = new ProgressDialog(MainActivity.this);
+        private String[] mNamesArray;
+        private Bitmap[] mBitmapImageArray;
+        private String[] mStatusArray;
+        private String[] mGenderArray;
+        private String[] mAgeArray;
+        private String[] mBreedArray;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog.setTitle("Looking For Animals Now!");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                // Connect to the Humane Society Website
+                Document document = Jsoup.connect(mURLAddress).get();
+
+                // This is done to get the number of animals so that arrays can be sized properly
+                Elements nameSelector = document.select(".animal-listing__title > a");
+                mAnimalCount = nameSelector.size();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            mProgressDialog.dismiss();
+            Toast.makeText(MainActivity.this, Integer.toString(mAnimalCount), Toast.LENGTH_LONG).show();
+        }
     }
 }
